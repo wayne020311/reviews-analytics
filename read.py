d9532c4983504f62dccc55e275888bdf252cabe6
Wayne <wayne020311@gmail.com>
2019-08-26 23:05:47 +0800

    caculate data

--- read.py
+++ read.py
@@ -4,5 +4,11 @@ with open("reviews.txt", 'r') as f:
 	for line in f:
 		data.append(line)
 		count  += 1
-		if count % 1000 == 0:
-			print(len(data))
+		# if count % 1000 == 0:
+			# print(len(data))
+print('檔案讀取完了，共有', len(data), '筆資料')
+x=0
+for d in data:
+	x = len(d) + x
+avg = x / len(data)
+print('留言平均長度為:', avg)
